f6d409841607ba78147a0f97630a65baa623a8e3
ybcoder01 <theman_mohit@proton.me>
2026-08-04 15:05:06 +0400

    Fix site metadata update

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -50,26 +50,3 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   );
 }
-import "./globals.css";
-import "@rainbow-me/rainbowkit/styles.css";
-import type { Metadata } from "next";
-import { Nav } from "../components/Nav";
-import { Providers } from "../components/Providers";
-
-export const metadata: Metadata = {
-  title: "XDCID",
-  description: ".XDC names for XDC"
-};
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>
-        <Providers>
-          <Nav />
-          {children}
-        </Providers>
-      </body>
-    </html>
-  );
-}
