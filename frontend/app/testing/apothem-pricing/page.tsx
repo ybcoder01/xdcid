f0a4135e22034c4ca89d60505339e9d1c1ceb41a
ybcoder01 <theman_mohit@proton.me>
2026-08-12 15:06:39 +0400

    Remove temporary Apothem test surface

--- frontend/app/testing/apothem-pricing/page.tsx
+++
@@ -1,9 +0,0 @@
-import { notFound } from "next/navigation";
-import ApothemPricingTestClient from "./ApothemPricingTestClient";
-
-export const dynamic = "force-dynamic";
-
-export default function ApothemPricingTestPage() {
-  if (process.env.ENABLE_APOTHEM_PRICING_TEST !== "true") notFound();
-  return <ApothemPricingTestClient />;
-}
